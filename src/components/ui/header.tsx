c9ce0b93c0b1c13c756bbb5e9c130d3f307b3b3f
Vinicius <yinioliveira@gmail.com>
2025-04-20 01:49:06 -0300

    feat: posicionei os elementos de logout na interface

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import Image from "next/image"
-import { ChevronDown } from "lucide-react"
+import { ChevronDown, LogOut, UserPen } from "lucide-react"
+import { useAuth } from "@/hooks/use-auth"
+import { Popover, PopoverTrigger, PopoverContent } from "./popover"
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 function Header() {
+    const { user, logout } = useAuth()
+    const router = useRouter()
+
+    const handleLogout = async () => {
+        try {
+            await logout()
+            router.push("/auth/login")
+        } catch (error) {
+            toast.error(`Erro ao fazer logout: ${error}`)
+        }
+    }
+
     return (
         <>
             <header className="bg-white w-full px-12 py-4 flex items-center justify-between border-b border-zinc-200">
@@ -17,13 +35,31 @@ function Header() {
                         alt="Avatar"
                         width={36}
                         height={36}
-                    //lassName="border border-primary rounded-full"
                     />
                     <div className="flex flex-col">
                         <span className="text-sm font-semibold text-zinc-700">Bem-vindo(a)</span>
-                        <div className="flex items-end gap-1 ">
-                            <span className="text-sm font-bold text-zinc-800">Nome do usuário</span>
-                            <ChevronDown size={16} className="" />
+                        <div className="">
+                            <Popover>
+                                <PopoverTrigger className="flex items-end gap-1 cursor-pointer">
+                                    <span className="text-sm font-bold text-zinc-800">{user?.name ?? "Nome do usuário"}</span>
+                                    <ChevronDown size={16} className="" />
+                                </PopoverTrigger>
+                                <PopoverContent className="w-[--radix-popover-trigger-width] min-w-[150px]">
+                                    <div className="flex flex-col gap-2">
+                                        <span className="flex items-center gap-2 text-sm font-semibold text-zinc-700 px-2 py-1 hover:bg-muted-foreground/10 rounded-md cursor-pointer">
+                                            <UserPen size={16} />
+                                            Perfil
+                                        </span>
+                                        <span
+                                            className="flex items-center gap-2 text-sm font-semibold text-zinc-700 px-2 py-1 hover:bg-muted-foreground/10 rounded-md cursor-pointer"
+                                            onClick={handleLogout}
+                                        >
+                                            <LogOut size={16} />
+                                            Sair
+                                        </span>
+                                    </div>
+                                </PopoverContent>
+                            </Popover>
                         </div>
                     </div>
                 </div>
