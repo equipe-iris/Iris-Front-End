562134549c5cc660a1f75fc0c43e6bb89e4cc4fd
Vinicius <yinioliveira@gmail.com>
2025-04-27 17:45:51 -0300

    feat: filtro de tempo no gráfico de tempo médio

--- src/features/(dashboard)/charts/average-time-handling/api/get-average-time-handling.ts
+++ src/features/(dashboard)/charts/average-time-handling/api/get-average-time-handling.ts
@@ -1,18 +1,28 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
-import { api } from "@/lib/api-client";
-import { AHTChartData } from "@/types/charts";
 
-function getAHT(): Promise<AHTChartData[]> {
-    return api.get(`/dashboard/average-service-time`);
+import { api } from "@/lib/api-client";
+import { getDateRange } from "@/lib/utils";
+
+import { AHTChartData } from "@/types/charts";
+import { TimeRange } from "@/types/api";
+
+function getAHT(timeRange: TimeRange): Promise<AHTChartData[]> {
+    const { start_date, end_date } = getDateRange(timeRange);
+    return api.get(`/dashboard/average-service-time`, {
+        params: {
+            start_date: start_date,
+            end_date: end_date
+        }
+    });
 }
 
-function getAHTQueryOptions() {
+function getAHTQueryOptions(timeRange: TimeRange) {
     return queryOptions({
-        queryKey: ["average-handling-time"],
-        queryFn: getAHT,
+        queryKey: ["average-handling-time", timeRange],
+        queryFn: () => getAHT(timeRange),
     })
 }
 
-export function useAHT() {
-    return useQuery(getAHTQueryOptions())
+export function useAHT(timeRange: TimeRange) {
+    return useQuery(getAHTQueryOptions(timeRange))
 }
