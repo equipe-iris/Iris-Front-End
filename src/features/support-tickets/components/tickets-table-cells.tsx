7cd243552fd386fe45d83466e02242e67b805451
Vinicius <yinioliveira@gmail.com>
2025-04-26 22:28:01 -0300

    fix: pequena correção na exibição das classificações dos chamados na listagem

--- src/features/support-tickets/components/tickets-table-cells.tsx
+++ src/features/support-tickets/components/tickets-table-cells.tsx
@@ -1,30 +1,32 @@
 import { Badge } from "@/components/ui/badge";
 
 const categoryBadgeClass: Record<string, string> = {
-    "Dúvida": "bg-teal-500",
-    "Reclamação": "bg-indigo-500",
-    "Suporte": "bg-cyan-500"
+    "duvida": "bg-teal-500",
+    "reclamacao": "bg-indigo-500",
+    "solicitacao": "bg-cyan-500"
 };
 export function TicketCategoryCell({ value }: { value: string }) {
     const badgeClass = categoryBadgeClass[value] ?? "bg-gray-500";
+    const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
 
     return (
-        <Badge variant="default" className={badgeClass}>{value}</Badge>
+        <Badge variant="default" className={badgeClass}>{capitalizedValue}</Badge>
     );
 }
 
 // ---------------------------------
 
 const emotionBadgeClass: Record<string, string> = {
-    "Positivo": "bg-green-500/10 text-green-600 border-green-600",
-    "Neutro": "bg-yellow-500/10 text-yellow-600 border-yellow-600",
-    "Negativo": "bg-red-500/10 text-red-600 border-red-600",
+    "positivo": "bg-green-500/10 text-green-600 border-green-600",
+    "neutro": "bg-yellow-500/10 text-yellow-600 border-yellow-600",
+    "negativo": "bg-red-500/10 text-red-600 border-red-600",
 }
 export function TicketEmotionCell({ value }: { value: string }) {
     const badgeClass = emotionBadgeClass[value] ?? "bg-gray-500";
+    const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
 
     return (
-        <Badge variant="outline" className={badgeClass} >{value}</Badge>
+        <Badge variant="outline" className={badgeClass} >{capitalizedValue}</Badge>
     )
 }
 
